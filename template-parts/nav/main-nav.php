27a4c7ed33496de7b7ca5723261b2199c42f7126
Tim Poerschke <info@coder-tim.de>
2019-10-30 17:05:42 +0100

    [BUGFIX] Fix animation of main navigation

--- template-parts/nav/main-nav.php
+++ template-parts/nav/main-nav.php
@@ -1,15 +1,15 @@
 <?php if ( has_nav_menu( 'menu-1' ) ) : ?>
-    <nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-light" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+    <nav id="site-navigation" class="main-navigation navbar navbar-expand-md navbar-light" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarToggle">
+        <div class="collapse navbar-collapse" id="navbarMain">
         <?php
         wp_nav_menu(
             array(
                 'theme_location' => 'menu-1',
                 'container'      => false,
-                'items_wrap'     => '<ul id="%1$s" class="nav flex-column nav-pills %2$s">%3$s</ul>',
+                'items_wrap'     => '<ul id="%1$s" class="navbar-nav flex-column nav-pills %2$s">%3$s</ul>',
                 'link_before'    => '',
                 'link_after'     => '',
                 'depth'          => 1,
