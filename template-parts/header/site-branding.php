958620c1d229dc3ec06f80197ec022e12f7ac097
Tim Poerschke <info@coder-tim.de>
2019-10-30 17:48:11 +0100

    [TASK] Site branding is responsive now

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -7,7 +7,7 @@
  * @since 1.0.0
  */
 ?>
-<div class="site-branding d-flex align-items-center">
+<div class="site-branding d-sm-flex align-items-center">
 
 	<?php if ( has_custom_logo() ) : ?>
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
