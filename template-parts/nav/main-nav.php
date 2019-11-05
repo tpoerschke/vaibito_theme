dde9108a3983f811656d5685b5cdc6ebee7eda9b
Tim Poerschke <info@coder-tim.de>
2019-11-05 21:06:43 +0100

    [TASK] Minor tweaks

--- template-parts/nav/main-nav.php
+++ template-parts/nav/main-nav.php
@@ -1,5 +1,5 @@
 <?php if ( has_nav_menu( 'menu-1' ) ) : ?>
-    <nav id="site-navigation" class="main-navigation navbar navbar-expand-md navbar-light" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
+    <nav id="site-navigation" class="main-navigation navbar navbar-expand-md navbar-light px-0" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
